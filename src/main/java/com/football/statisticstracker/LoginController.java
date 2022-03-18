fac4b5a9ca10326226ae189cb0d8a0e767a50394
vk1cd314 <drakensang47@gmail.com>
2022-03-18 22:30:06 +0600

    Basic Login UI

--- src/main/java/com/football/statisticstracker/LoginController.java
+++ src/main/java/com/football/statisticstracker/LoginController.java
@@ -1,14 +1,37 @@
 package com.football.statisticstracker;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-public class LoginController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class LoginController implements Initializable {
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label loginStatus;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private PasswordField loginPassword;
+    @FXML
+    private TextField loginUsername;
+
+    public void onLoginClickActionEvent(ActionEvent event) {
+        //try {
+        ////    to be done
+        //} catch() {
+        //
+        //}
+    //    to be done
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
--- src/main/java/com/football/statisticstracker/LoginMain.java
+++ src/main/java/com/football/statisticstracker/LoginMain.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class LoginMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LoginMain.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginMain.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
     }
