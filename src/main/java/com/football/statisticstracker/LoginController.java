cbf345d33fcd72320f00365724d8d7b2a5ed0dd8
vk1cd314 <drakensang47@gmail.com>
2022-03-18 21:01:17 +0600

    Adding Initial Files

--- src/main/java/com/football/statisticstracker/LoginController.java
+++ src/main/java/com/football/statisticstracker/LoginController.java
@@ -3,7 +3,7 @@ package com.football.statisticstracker;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LoginController {
     @FXML
     private Label welcomeText;
 
