3e8a0138a9b575b1456d5d9d25c977b97634fc6d
compu <muztoba.sinha@gmail.com>
2022-03-31 05:38:06 +0600

    added rest player classes

---
+++ src/main/java/com/football/statisticstracker/Goalkeeper.java
@@ -0,0 +1,8 @@
+package com.football.statisticstracker;
+
+public class Goalkeeper extends Player{
+
+    public Goalkeeper(String pos, String name, int age, String birthday, String league, String club, String nation, int app, int goals, int ass, int cs, int rc, int yc) {
+        super("Goalkeeper", name, age, birthday, league, club, nation, app, goals, ass, cs, rc, yc);
+    }
+}
