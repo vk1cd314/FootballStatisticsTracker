d2835fd9d9b3874e1fb554dd222246db3652ef03
vk1cd314 <drakensang47@gmail.com>
2022-04-13 02:53:34 +0600

    Changed Dashboard and LeagueView

--- src/main/java/dashboard/DashboardHboxController.java
+++ src/main/java/dashboard/DashboardHboxController.java
@@ -1,5 +1,67 @@
 package dashboard;
 
-public class DashboardHboxController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.Objects;
+
+public class DashboardHboxController {
+    @FXML
+    private Button cross;
+    @FXML
+    private HBox tilesContainer;
+
+    public void initialize() {
+        HBox tile1 = null, tile2  = null;
+        try {
+            tile1 = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("tile1.fxml")));
+            tile2 = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("tile2.fxml")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //if (this.tilesContainer == null) System.out.println("chudi life ke");
+        //System.out.println("Gets Here");
+        this.tilesContainer.getChildren().add(tile1);
+        //System.out.println("Gets Here");
+        this.tilesContainer.getChildren().add(tile2);
+        //System.out.println("Gets Here");
+    }
+
+    public void dashboardStart(BorderPane borderPane) {
+        //HBox tile1 = null, tile2  = null;
+        //try {
+        //    tile1 = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("tile1.fxml")));
+        //    tile2 = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("tile2.fxml")));
+        //} catch (IOException e) {
+        //    e.printStackTrace();
+        //}
+        ////if (this.tilesContainer == null) System.out.println("chudi life ke");
+        //System.out.println("Gets Here");
+        //this.tilesContainer.getChildren().add(tile1);
+        //System.out.println("Gets Here");
+        //this.tilesContainer.getChildren().add(tile2);
+        //System.out.println("Gets Here");
+        //Do nothing? not anymore lul
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("dashboardHboxFXML.fxml")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        borderPane.setCenter(root);
+    }
+
+    public void quit() {
+        //DashboardController dashboardController = new DashboardController();
+        //dashboardController.quit();
+        Stage stage = (Stage) this.cross.getScene().getWindow();
+        stage.close();
+    }
 }
